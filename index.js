7f1496afc71630d111b2f29e29800175de2191d0
galvisgrjp <galvisgrjp@gmail.com>
2022-03-03 18:03:42 -0500

    complete exercise

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 const fs = require('fs');
-const items = null;
+var respArr = [];
 
-fs.readFile('products-ecommerce.json', 'utf-8', (err, data) => {
-  items = data;
+var rawData = fs.readFileSync('data/products-ecommerce.json');
+var data = JSON.parse(rawData);
+
+for (let i = 0; i < data.items.length; i++) {
+  let item = data.items[i];
+  if (item.price.amount < 10000)
+    respArr.push(item);
+}
+
+var resp = "";
+respArr.forEach(item => {
+  let str = JSON.stringify(item, null, 2);
+  resp += str + ",\n";
 })
+
+try {
+  fs.writeFileSync("data/products-filtered.json", resp);
+  console.log("File written successfully\n");
+} catch(err) {
+  console.log(err);
+}
